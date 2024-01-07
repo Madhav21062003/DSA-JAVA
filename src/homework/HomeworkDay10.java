8c0fd9dd9664967a3d0bf8991dadcb3b371a17d3
Madhav <madhav09solanki@gmail.com>
2024-01-07 18:52:44 +0530

    Hard Questions added

--- src/homework/HomeworkDay10.java
+++ src/homework/HomeworkDay10.java
@@ -5,9 +5,33 @@ public class HomeworkDay10 {
 
 
         // Q-1
+        int n = 5 ;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n-row; col++) {
+                System.out.print(" ");
+            }
+                for (int col = row; col >= 1; col--) {
+                    System.out.print(col);
+                }
+            System.out.println();
+        }
 
 
         // Q-2
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print characters
+            for (int k = 1; k <= i; k++) {
+                System.out.print((char) (64 + i) );
+            }
+
+            // Move to the next line after each row
+            System.out.println();
+        }
 
         // Q-3
 
