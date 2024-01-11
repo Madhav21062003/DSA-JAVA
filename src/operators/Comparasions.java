402221b2be29da61436437aeb9b423ff1e665fc3
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-11 20:52:34 +0530

    Operators

---
+++ src/operators/Comparasions.java
@@ -0,0 +1,31 @@
+package operators;
+
+public class Comparasions {
+    public static void main(String[] args) {
+
+        // Comparison Operators are:  =, ==, >, <, >=, <=, !=
+        int a = 7;
+        int b = a;
+
+        // Use of = operator
+        System.out.println(b);
+
+        // Use of == operator
+        System.out.println(a==b);
+
+        // Use of > operator
+        System.out.println(a > b);
+
+        // Use of < operator
+        System.out.println(a < b);
+
+        // Use of >= operator
+        System.out.println(a >= b);
+
+        // Use of <= operator
+        System.out.println(a <= b);
+
+        // Use of != operator
+        System.out.println(a != b);
+    }
+}
