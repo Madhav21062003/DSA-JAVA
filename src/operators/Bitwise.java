485f9c5866e8b45197a0926e8a0987306e52b2b2
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-11 21:25:49 +0530

    Bitwise Operators

---
+++ src/operators/Bitwise.java
@@ -0,0 +1,24 @@
+package operators;
+
+public class Bitwise {
+    public static void main(String[] args) {
+
+        // Bitwise Operators are: &, |, ^, <<, >>
+
+        // & operator
+        int ans = 2 & 3;
+        System.out.println(ans);
+
+        ans = 2 | 3;
+        System.out.println(ans);
+
+        ans = 2 << 3;
+        System.out.println(ans);
+
+        ans = 2 >> 3;
+        System.out.println(ans);
+
+        ans = 2 ^ 3;
+        System.out.println(ans);
+    }
+}
