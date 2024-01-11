ba2d58bcc372a7c915dd57436c17ccd3be9218da
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-11 20:48:43 +0530

    Operators

---
+++ src/operators/Introduction.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class Introduction {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+        // a) Unary b) Binary  --> +, -, *, /, %
+
+        int num = 5;
+
+        // Post Increment
+
+        System.out.println(num++);
+        System.out.println(num);
+
+
+        // Pre Increment
+        int n = 7;
+        System.out.println(++n);
+    }
+}
