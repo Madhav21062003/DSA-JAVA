9a0a0911f8895fed69b68eebd549d419e21c2556
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-12 21:39:11 +0530

    Homework

--- src/homework/HomeworkDay15.java
+++ src/homework/HomeworkDay15.java
@@ -49,6 +49,60 @@ public class HomeworkDay15 {
         // 4: Given a number n, print the corresponding month of it. For n=1, print Jan, n=2, print Feb…,
         // if the user puts any invalid number, don’t do anything. (Use switch here)
 
+        int month = 7;
+        switch (month){
+            case 1:
+                System.out.print("JAN");
+                break;
+
+            case 2:
+                System.out.print("FEB");
+                break;
+
+            case 3:
+                System.out.print("MARCH");
+                break;
+
+            case 4:
+                System.out.print("APRIL");
+                break;
+
+            case 5:
+                System.out.print("MAY");
+                break;
+
+            case 6:
+                System.out.print("JUNE");
+                break;
+
+            case 7:
+                System.out.print("JULY");
+                break;
+
+            case 8:
+                System.out.print("AUG");
+                break;
+
+            case 9:
+                System.out.print("SEPT");
+                break;
+
+            case 10:
+                System.out.print("OCT");
+                break;
+
+            case 11:
+                System.out.print("NOV");
+                break;
+
+            case 12:
+                System.out.print("DEC");
+                break;
+
+            default:
+                System.out.print("Invalid Month Number");
+                break;
+        }
 
         // 5: Print all the Capital and small letters  using a while loop. It means A-Z, then a-z.
 
