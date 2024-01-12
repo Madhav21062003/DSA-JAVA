0da392fd74735cf302077f7b7f7586cfda71955e
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-12 19:58:41 +0530

    whileloops

---
+++ src/loops/whileloops/WhileLoops.java
@@ -0,0 +1,13 @@
+package loops.whileloops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
