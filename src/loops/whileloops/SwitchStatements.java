459db6d1bb1affc589e144c674eeb8de164b2ded
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-12 21:20:43 +0530

    whileloops

---
+++ src/loops/whileloops/SwitchStatements.java
@@ -0,0 +1,54 @@
+package loops.whileloops;
+
+public class SwitchStatements {
+    public static void main(String[] args) {
+
+        char vowel = 'j';
+
+        switch (vowel){
+            case 'a':
+                System.out.println("Vowel");
+                break;
+
+            case 'e':
+                System.out.println("Vowel");
+                break;
+
+            case 'i':
+                System.out.println("Vowel");
+                break;
+
+            case 'o':
+                System.out.println("Vowel");
+                break;
+
+            case 'u':
+                System.out.println("Vowel");
+                break;
+
+            case 'A':
+                System.out.println("Vowel");
+                break;
+
+            case 'E':
+                System.out.println("Vowel");
+                break;
+
+            case 'I':
+                System.out.println("Vowel");
+                break;
+
+            case 'O':
+                System.out.println("Vowel");
+                break;
+
+            case 'U':
+                System.out.println("Vowel");
+                break;
+
+            default:
+                System.out.println("Consonent");
+                break;
+        }
+    }
+}
