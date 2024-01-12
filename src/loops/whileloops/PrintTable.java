fe86f480cf842a78af7a6fb74a231cb410db7d65
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-12 20:00:43 +0530

    whileloops

---
+++ src/loops/whileloops/PrintTable.java
@@ -0,0 +1,15 @@
+package loops.whileloops;
+
+import java.util.Scanner;
+
+public class PrintTable {
+    public static void main(String[] args) {
+
+        int  n = 2;
+        int i = 1;
+        while ( i <= 10){
+            System.out.println(n * i);
+            i++;
+        }
+    }
+}
