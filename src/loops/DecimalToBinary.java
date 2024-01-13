7894d5bc54dee4fffc78d29205255cb131fc6dd5
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-13 22:19:49 +0530

    Basic Questions

--- src/loops/DecimalToBinary.java
+++ src/loops/DecimalToBinary.java
@@ -8,7 +8,7 @@ public class DecimalToBinary {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Decimal Number: ");
         int num = sc.nextInt();
-
+        int num_2 = 13;
         int rem, ans = 0, mul = 1;
 
         while (num > 0){
@@ -18,7 +18,18 @@ public class DecimalToBinary {
             mul = mul * 10;   // Update power of 10
         }
 
+        int rem_2, ans_2 = 0;
+
+        // UsingBitwise Operator
+        while (num_2 > 0){
+            rem_2 = num_2 & 1; // Remainder i.e mod by  `2
+            num_2 = num_2 >> 1 ;  // Quotient i.e num / 2
+            ans_2 = rem_2 * mul + ans_2;
+            mul = mul * 10;
+        }
+
         System.out.println("Your Binary Number is: "+ans);
+        System.out.println("Your Binary Number is: "+ans_2);
 
 
     }
