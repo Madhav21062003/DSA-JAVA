ae02650f90a0354c169e719f0249780d3f88ca99
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-17 10:24:57 +0530

    Online Platform Problems

---
+++ src/online_platform_problems/Palindrome.java
@@ -0,0 +1,31 @@
+package online_platform_problems;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num  = sc.nextInt();
+        int x = num;
+        int ans = 0, rem;
+
+        if (x < 0){
+            System.out.println("Not Palindrome");
+        } else if (x >= 0 && x <= 9) {
+            System.out.println("Palindrome");
+        }
+        else {
+            while (x != 0) {
+                rem = x % 10;
+                ans = ans * 10 + rem;
+                x = x / 10;
+            }
+            if (ans == num)
+                System.out.println("Palindrome");
+            else
+                System.out.println("Not Palindrome");
+        }
+    }
+}
