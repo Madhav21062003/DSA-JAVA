b9260ee7fd21a94778f16ea6a3cbf02a5f77fc4f
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-17 10:15:46 +0530

    Online Platform Problems

--- src/online_platform_problems/SqrtOfX.java
+++ src/online_platform_problems/SqrtOfX.java
@@ -3,6 +3,16 @@ package online_platform_problems;
 public class SqrtOfX {
     public static void main(String[] args) {
 
+        int num = 111;
+        int i = 1;
+        int ans  = 1;
+        while (ans <= num){
+//            ans = i * i ;
+            i++;
+            ans = i * i;
+        }
+        System.out.println(i-1);
+
     }
     
 }
