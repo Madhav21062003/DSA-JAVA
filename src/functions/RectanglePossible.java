a5056c70d1623d3b6301ff22c396b51e07b90636
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-20 23:20:55 +0530

    Functions

---
+++ src/functions/RectanglePossible.java
@@ -0,0 +1,19 @@
+package functions;
+
+public class RectanglePossible {
+    public static void main(String[] args) {
+
+        boolean ans = rectanglePossible(1,2,1,2);
+        System.out.println(ans);
+    }
+
+    static boolean rectanglePossible(int a, int b, int c, int d){
+        if (a == b && a==c && a == d){
+            return false;
+        }
+        else if ((a == b && c == d) || (a == c && b == d) || (a == d && b == c))
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/functions/TrailingZeros.java
@@ -0,0 +1,19 @@
+package functions;
+
+public class TrailingZeros {
+    public static void main(String[] args) {
+
+        int result = trailingZeros(100);
+        System.out.println(result);
+
+    }
+    static int trailingZeros(int num){
+
+        int count = 0;
+       while (num >= 5){
+           count = count + num / 5;
+           num = num / 5;
+       }
+       return count;
+    }
+}
