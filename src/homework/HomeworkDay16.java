0999e8c1a5456c9cd7adb05827f59b6dac1cf0ff
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-20 19:03:17 +0530

    Homework Day 16

--- src/homework/HomeworkDay16.java
+++ src/homework/HomeworkDay16.java
@@ -4,8 +4,17 @@ public class HomeworkDay16 {
     public static void main(String[] args) {
 
         // Write a program to convert binary numbers to decimal numbers using a for loop.
+            int rem, ans =0, mul = 1, n = 111;
+            while (n != 0){
+                rem = n % 2;
+                n = n / 10;
+                ans = ans + rem * mul;
+                mul = mul * 2;
+            }
+        System.out.println(ans);
 
         // Write a program to convert decimal numbers to binary numbers using a for loop.
+
 
         // Write a program to convert decimal numbers to Octal numbers.
 
