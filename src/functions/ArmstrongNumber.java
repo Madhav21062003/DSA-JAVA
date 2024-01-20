e629a46a38eaff7d62b883c059ca93c7bdc82a48
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-20 22:41:43 +0530

    Functions

--- src/functions/ArmstrongNumber.java
+++ src/functions/ArmstrongNumber.java
@@ -1,5 +1,29 @@
 package functions;
 
-public class ArmstrongNumber {
+import java.util.Scanner;
 
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean result = armstrongNum(153);
+        System.out.println(result);
+        System.out.println(armstrongNum(154));
+    }
+
+    static boolean armstrongNum(int num){
+        int rem;
+        int sum = 0;
+        int x = num;
+
+        while (x != 0){
+            rem = x % 10;
+            x = x / 10;
+            sum = sum + (rem*rem*rem);
+        }
+        if (sum == num)
+            return true;
+        else
+            return false;
+    }
 }
