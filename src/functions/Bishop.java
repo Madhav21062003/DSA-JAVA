4ee043e5f446e2dd3dc3cc58da2351a2171e6e0c
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-22 11:01:36 +0530

    Functions

--- src/functions/Bishop.java
+++ src/functions/Bishop.java
@@ -3,5 +3,16 @@ package functions;
 public class Bishop {
     public static void main(String[] args) {
 
+
+    }
+
+    static int minBishopMoves(int A, int B){
+        int count = 0;
+       count = count + Math.min(8-A, 8-B);
+       count = count + Math.min(8-A, B-1);
+       count = count + Math.min(A-1, B-1);
+       count = count + Math.min(A-1, 8-B);
+
+       return count;
     }
 }
