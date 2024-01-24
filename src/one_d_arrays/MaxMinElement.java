5b53a51c807d754dc3c8890d6d252daab7c5198e
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-24 23:35:43 +0530

    Arrays

--- src/one_d_arrays/MaxMinElement.java
+++ src/one_d_arrays/MaxMinElement.java
@@ -3,5 +3,18 @@ package one_d_arrays;
 public class MaxMinElement {
     public static void main(String[] args) {
 
+//        int[] arr = {1,8,6,6,99,5,555} ;
+        maxMinElement(new int[]{1,8,6,6,99,5,555});
+    }
+
+    static void maxMinElement(int[] arr){
+        int maxElement = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxElement){
+                maxElement = arr[i];
+            }
+        }
+        System.out.println(maxElement);
     }
 }
