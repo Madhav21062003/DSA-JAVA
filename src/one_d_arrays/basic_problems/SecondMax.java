0894c7a96bc9c0a572972a0e72dd47b2d853b5ba
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-25 12:21:22 +0530

    Arrays

--- src/one_d_arrays/basic_problems/SecondMax.java
+++ src/one_d_arrays/basic_problems/SecondMax.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SecondMax {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 16, 7, 8, 98};
         int result = secondMaxElement(arr);
         System.out.println(result);
     }
@@ -17,8 +17,8 @@ public class SecondMax {
     //  Using Sorting Method in java
     private static int secondMaxElement(int[] arr){
         Arrays.sort(arr);
-        int secondMax = arr[arr.length-2];
 
-        return secondMax;
+
+        return arr[arr.length-2];
     }
 }
