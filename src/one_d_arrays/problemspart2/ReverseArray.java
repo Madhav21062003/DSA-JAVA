155f5d9f1f84f230d07c0289db2232525e707789
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-25 12:03:45 +0530

    Arrays

--- src/one_d_arrays/problemspart2/ReverseArray.java
+++ src/one_d_arrays/problemspart2/ReverseArray.java
@@ -6,13 +6,29 @@ public class ReverseArray {
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        reverseArray(arr);
-        System.out.println();
-        System.out.println(Arrays.toString(reverseArrayMethod2(arr)));
-
-        reverseArrayMethod3(arr);
+//        reverseArray(arr);
+//        System.out.println();
+//        System.out.println(Arrays.toString(reverseArrayMethod2(arr)));
+        int []temp = reverseArr(arr);
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(temp[i]+" ");
+        }
+//        reverseArrayMethod3(arr);
     }
 
+    static int[] reverseArr(int[]arr){
+        int j = 0;
+        int temp;
+        for (int i = arr.length-1; i >0 ; i--) {
+            if (i > j){
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                j++;
+            }
+        }
+        return arr;
+    }
     static void reverseArray(int[] arr) {
 
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -32,25 +48,5 @@ public class ReverseArray {
         return ans;
     }
 
-    static void reverseArrayMethod3(int[]arr){
-        int i = 0;
-        int j = arr.length-1;
 
-        while (i < j){
-            swapArray(arr, i, j);
-            i++;
-            j--;
-        }
-
-        for (int k = 0; k < arr.length; k++) {
-            System.out.print(arr[k]+"  ");
-        }
-
-    }
-
-    static  void swapArray(int[]arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp ;
-    }
 }
