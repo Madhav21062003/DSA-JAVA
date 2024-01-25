f7982937f0cc0b7a9e26a19676b77cf9ab95b573
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-25 23:19:28 +0530

    Arrays

--- src/one_d_arrays/basic_problems/FibonacciSeries.java
+++ src/one_d_arrays/basic_problems/FibonacciSeries.java
@@ -1,7 +1,21 @@
 package one_d_arrays.basic_problems;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        int[] arr = new int[1000];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        System.out.println(n+"th Fibonacci Number = "+arr[n-1]);
     }
 }
