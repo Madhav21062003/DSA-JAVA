ed680bd3c3beb4db084b3cbf1982cb5926f4ffe6
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-25 11:40:43 +0530

    Arrays

--- src/one_d_arrays/LinearSearch.java
+++ src/one_d_arrays/LinearSearch.java
@@ -4,10 +4,15 @@ public class LinearSearch {
     public static void main(String[] args) {
 
         int[] arr = {1,8,6,68,99,5,555} ;
-        int key = 68 ;
+        int key = 684 ;
 
         System.out.println(linerSearch(arr, key));
 
+        int result = searchElement(arr, key);
+        if (result == -1)
+            System.out.println(key+" is Not present in the array");
+        else
+            System.out.println(key+" is present in the array at index: "+result);
     }
 
     static int linerSearch(int[]arr, int key){
@@ -18,6 +23,17 @@ public class LinearSearch {
         }
 
         return -1;
+    }
+
+    static int searchElement(int[] arr, int key){
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key){
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
 }
