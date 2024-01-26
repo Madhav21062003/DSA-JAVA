e8e2d33d166007d4064b72ab09376d8f9f93da4c
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-26 23:33:13 +0530

    Arrays

--- src/one_d_arrays/basic_problems/CyclicallyRotateAnArrayByOne.java
+++ src/one_d_arrays/basic_problems/CyclicallyRotateAnArrayByOne.java
@@ -14,7 +14,7 @@ public class CyclicallyRotateAnArrayByOne {
         int temp = arr[n-1];
 
         for (int i = n-2; i >=0 ; i--) {
-            arr[i+1] = arr[i];
+             arr[i+1] = arr[i];
         }
         arr[0] = temp;
 
