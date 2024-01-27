870599dd2b2f1036d6bbab09060f33cc85d73145
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-27 20:36:37 +0530

    Homework

--- src/homework/HomeworkDay17.java
+++ src/homework/HomeworkDay17.java
@@ -10,5 +10,8 @@ public class HomeworkDay17 {
 
         // 3: There are three numbers a,b,c.
         // Put the value of a into b, put value of b into c and put value of c into a. Do it using Function.
+
+        // 4: Swap 2 numbers a, b without using extra variables. Range of
+        //  -10000<=a,b<=100000.
     }
 }
