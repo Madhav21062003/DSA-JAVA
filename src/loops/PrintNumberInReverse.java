f1559b2ff8612db608d096a312868e44fbb22114
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-28 23:35:15 +0530

    Homework

--- src/loops/PrintNumberInReverse.java
+++ src/loops/PrintNumberInReverse.java
@@ -6,6 +6,7 @@ public class PrintNumberInReverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Here we print the reverse of the given number
         System.out.println("Enter Number: ");
         int n = sc.nextInt();
         for (int i = n; i > 0 ; i--) {
