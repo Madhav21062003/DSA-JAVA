b66edb221e3bb17a8e72f62b42a305c20a06d6f7
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-29 22:42:54 +0530

    Homework

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -10,23 +10,24 @@ public class SelectionSort {
         }
     }
 
+    // In Ascending Order
     static void selectionSort(int[] arr) {
         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {    // i represent the current index
-
-            // find the minimum element in unsorted part of array
-            int min_index = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min_index]) {
-                    min_index = j;
-                }
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            // Finding The smallest Element from the array
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j ;
             }
 
-            // swap current and minimum element -> current index i will have correct element
+            // Swap the minimum element with Greater element
             int temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
+
+
 }
