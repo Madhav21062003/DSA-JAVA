671e2e6ca31204774852687d106a8dfaa04afbd5
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-30 10:00:07 +0530

    Sorting

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -4,8 +4,9 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        bubbleSort(arr);
-        for (int i : arr) {
+        int[] arr1 = {1,2,3,4,5};
+        bubbleSort(arr1);
+        for (int i : arr1) {
             System.out.print(i + " ");
         }
     }
@@ -14,12 +15,18 @@ public class BubbleSort {
         int n = arr.length;
 
         for (int i = n - 2; i >= 0; i--) {
+
+            boolean swappingOccur = false;
             for (int j = 0; j <= i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swappingOccur = true;
                 }
+
+                if (!swappingOccur)
+                    return;
             }
         }
 
