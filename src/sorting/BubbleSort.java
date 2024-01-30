01e72ea286a576443a66cd4a42968a4214ea5253
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-30 09:50:37 +0530

    Sorting

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -13,12 +13,12 @@ public class BubbleSort {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
 
-        for (int i = n-2; i >= 0 ; i--) {
+        for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                if (arr[j] > arr[j+1]){
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]  = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
