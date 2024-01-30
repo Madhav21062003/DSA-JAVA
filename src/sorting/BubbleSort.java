a7d07bd7aad1b416b4aa451ca33682e59ac63cbe
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-30 09:50:16 +0530

    Sorting

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -13,23 +13,15 @@ public class BubbleSort {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
 
-        // n-1 iteration / passes
-        for (int i = 0; i < n - 1; i++) {
-            boolean flag = false;  // has any swapping happened
-            for (int j = 0; j < n - i - 1; j++) {
-                // last ith element
-                // nt ar correct sorted positions so no need to check them
-                if (arr[j] > arr[j + 1]) {
-                    // swap a[j] , a[j+1]
+        for (int i = n-2; i >= 0 ; i--) {
+            for (int j = 0; j <= i; j++) {
+                if (arr[j] > arr[j+1]){
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    flag = true; // it means some swaps happened
-                }
-                if (!flag){
-                    return;
+                    arr[j] = arr[j+1];
+                    arr[j+1]  = temp;
                 }
             }
         }
+
     }
 }
