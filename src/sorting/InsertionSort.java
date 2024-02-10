6cb38992e344321e967139904d54464fb1914dfe
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 11:01:05 +0530

    Sorting

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -2,7 +2,7 @@ package sorting;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arr = {101, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         insertionSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
