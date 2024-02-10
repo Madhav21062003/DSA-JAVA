a9159ba088bd863bcc2ded9dc16a74cf068975d7
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 14:09:17 +0530

    Homework Day 19

--- src/homework/HomeworkDay22.java
+++ src/homework/HomeworkDay22.java
@@ -3,5 +3,18 @@ package homework;
 public class HomeworkDay22 {
     public static void main(String[] args) {
 
+        int[] arr = {1,2,3,45,6};
+        System.out.println(sumOfElements(arr));
+
+    }
+
+    // 1: Take elements from user input and find its sum with the help of an array.
+    static int sumOfElements(int[] arr){
+        int sum = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        return sum;
     }
 }
--- src/homework/HomeworkDay29.java
+++ src/homework/HomeworkDay29.java
@@ -3,5 +3,8 @@ package homework;
 public class HomeworkDay29 {
     public static void main(String[] args) {
 
+
     }
+
+
 }
