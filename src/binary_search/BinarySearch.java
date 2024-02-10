72f8ea04ec443f9ce49520317a123c65260f8abc
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 16:54:40 +0530

    Binary Search

--- src/binary_search/BinarySearch.java
+++ src/binary_search/BinarySearch.java
@@ -6,6 +6,8 @@ public class BinarySearch {
         int[] arr = {2, 4, 5, 7, 24, 45, 50, 77};
         int key = 7;
 
+        int[] arrDes = {50,40,30,20,10};
+        System.out.println("The Element isPresent at Index "+binarySearchDecreasingOrder(arrDes, 30));
         int ans = binarySearch(arr, key);
         if (ans != -1)
             System.out.println("The Element is  present in the array at index " + ans);
@@ -27,6 +29,24 @@ public class BinarySearch {
 
             else
                 end = mid - 1;
+        }
+        return -1;
+    }
+
+    static int binarySearchDecreasingOrder(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (arr[mid] < target)
+                end = mid -1 ;
+
+            else
+                start = mid + 1;
         }
         return -1;
     }
