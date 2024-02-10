c0d23c8e955a6f8bce822243c403a9d902c67d96
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 14:13:20 +0530

    Homework Day 22

--- src/homework/HomeworkDay22.java
+++ src/homework/HomeworkDay22.java
@@ -6,6 +6,10 @@ public class HomeworkDay22 {
         int[] arr = {1,2,3,45,6};
         System.out.println(sumOfElements(arr));
 
+        int[] avgArr = new int[18];
+        avgArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+        System.out.println(calcAvg(avgArr));
+
     }
 
     // 1: Take elements from user input and find its sum with the help of an array.
@@ -14,7 +18,20 @@ public class HomeworkDay22 {
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-
         return sum;
     }
+
+//    2: Calculate the average of elements in an array of size 18.
+        static float calcAvg(int[] arr){
+            float avg ;
+            int sum = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                sum = sum + arr[i];
+            }
+
+            avg = sum / 18 ;
+
+            return avg;
+        }
 }
