750ae1beba9585c88928982c66b3c9c1c8bef90f
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 10:37:58 +0530

    Sorting

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -10,16 +10,6 @@ public class InsertionSort {
     }
 
     static void insertionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; i++) {
-            int j = i ;
-            while ( j > 0 && arr[j] < arr[j-1]){
-                //swap a[j], a[j-1]
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
-                j--;
-            }
-        }
+
     }
 }
