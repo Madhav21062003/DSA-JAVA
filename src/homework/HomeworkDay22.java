fc3b012ca31424f1a7310b5454734bbc8414125e
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 14:36:27 +0530

    Homework Day 22

--- src/homework/HomeworkDay22.java
+++ src/homework/HomeworkDay22.java
@@ -3,7 +3,7 @@ package homework;
 public class HomeworkDay22 {
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3,45,6};
+        int[] arr = {1, 2, 3, 45, 6};
         System.out.println(sumOfElements(arr));
 
         int[] avgArr = new int[18];
@@ -13,25 +13,46 @@ public class HomeworkDay22 {
     }
 
     // 1: Take elements from user input and find its sum with the help of an array.
-    static int sumOfElements(int[] arr){
-        int sum = 0 ;
+    static int sumOfElements(int[] arr) {
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
         return sum;
     }
 
-//    2: Calculate the average of elements in an array of size 18.
-        static float calcAvg(int[] arr){
-            float avg ;
-            int sum = 0;
+    //    2: Calculate the average of elements in an array of size 18.
+    static float calcAvg(int[] arr) {
+        float avg;
+        int sum = 0;
 
-            for (int i = 0; i < arr.length; i++) {
-                sum = sum + arr[i];
-            }
-
-            avg = sum / 18 ;
-
-            return avg;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
         }
+
+        avg = sum / 18;
+
+        return avg;
+    }
+
+    // 3: Find the index of a specific element in an array, if the element is nor present, print -1.
+    static int findElementIndex(int[] arr, int key){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key)
+                return i;
+        }
+
+        return -1;
+    }
+
+
+
+    // 4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
+
+    // 5: Find the second largest element in an array of unique elements of size n. Where n>3.
+
+
+    // 6: Find the third smallest element in an array of unique elements size n. Where n>3.
+
+    // 7: What is Byte addressable?
 }
