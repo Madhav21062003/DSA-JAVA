b4b9783e612e18022c38aec6b973496cde8a1cae
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-10 14:49:57 +0530

    Homework Day 22

--- src/homework/HomeworkDay22.java
+++ src/homework/HomeworkDay22.java
@@ -7,12 +7,13 @@ public class HomeworkDay22 {
 
         int[] arr = {1, 2, 3, 45, 6};
         System.out.println(sumOfElements(arr));
+        System.out.println(secondLargestElement(arr));
 
         int[] avgArr = new int[18];
         avgArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
         System.out.println(calcAvg(avgArr));
 
-        char[] ch = {'a','b','c','d','e','f'};
+        char[] ch = {'a', 'b', 'c', 'd', 'e', 'f'};
         displayChar(ch);
 
     }
@@ -41,7 +42,7 @@ public class HomeworkDay22 {
     }
 
     // 3: Find the index of a specific element in an array, if the element is nor present, print -1.
-    static int findElementIndex(int[] arr, int key){
+    static int findElementIndex(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key)
                 return i;
@@ -51,28 +52,45 @@ public class HomeworkDay22 {
     }
 
 
-
     // 4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
-        static void displayChar(char[] ch){
-            Scanner sc = new Scanner(System.in);
-            char[] ch1 = new char[ch.length];
+    static void displayChar(char[] ch) {
+        Scanner sc = new Scanner(System.in);
+        char[] ch1 = new char[ch.length];
 
-            // Store 'a' to 'z' in the array
-            for (int i = 0; i < ch.length; i++) {
-                ch1[i] = (char) ('a' + i);
-            }
-
-            // Print the elements of the array
-            System.out.println("Elements of the character array are: ");
-            for (char c : ch1){
-                System.out.print(c+" ");
-            }
+        // Store 'a' to 'z' in the array
+        for (int i = 0; i < ch.length; i++) {
+            ch1[i] = (char) ('a' + i);
         }
 
-    // 5: Find the second largest element in an array of unique elements of size n. Where n>3.
+        // Print the elements of the array
+        System.out.println("Elements of the character array are: ");
+        for (char c : ch1) {
+            System.out.print(c + " ");
+        }
+    }
 
+    // 5: Find the second largest element in an array of unique elements of size n. Where n>3.
+    static int secondLargestElement(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        // Step -1  find the maximum element of the array
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        // comapre one by one the elemet which ig greater than the rest of elemets excet max element
+        for (int i = 0; i < arr.length; i++) {
+            if (max != arr[i])
+                secondMax = Math.max(secondMax, arr[i]);
+        }
+
+        return secondMax;
+    }
 
     // 6: Find the third smallest element in an array of unique elements size n. Where n>3.
+
 
     // 7: What is Byte addressable?
 }
