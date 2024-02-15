530c98b474eada550e6494757fd0f57843244e03
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-15 12:03:51 +0530

    PrintSubArrays

---
+++ src/one_d_arrays/PrintSubArrays.java
@@ -0,0 +1,24 @@
+package one_d_arrays;
+
+public class PrintSubArrays {
+    public static void main(String[] args) {
+        int arr[] = { 10, 2, 3, 99, 12, 0 };
+        System.out.println("The subarrays are-");
+        printSubArrays(arr);
+    }
+
+    static void printSubArrays(int[] arr){
+        // For loop for start index
+        for (int i = 0; i < arr.length; i++)
+
+            // For loop for end index
+            for (int j = i; j < arr.length; j++) {
+
+                // For loop to print subarray elements
+                for (int k = i; k <=j; k++)
+                    System.out.print(arr[k] + " ");
+                System.out.println("");
+            }
+    }
+}
+
