47aa97f3f3c607273d8798b1e4e5de527a947816
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-16 10:03:20 +0530

    Kadanes Algorithm

--- src/homework/HomeworkDay39.java
+++ src/homework/HomeworkDay39.java
@@ -1,5 +1,9 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HomeworkDay39 {
     public static void main(String[] args) {
 
---
+++ src/one_d_arrays/LargestSumContigousSubArray.java
@@ -0,0 +1,40 @@
+package one_d_arrays;
+
+public class LargestSumContigousSubArray {
+    public static void main(String[] args) {
+
+    }
+
+    // Brute force approach
+
+    static int lagestContigosSubArraySum(int[] arr){
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            int prefix = 0;
+            for (int j = i; j < arr.length; j++) {
+                prefix += arr[j];
+                maxi = Math.max(maxi, prefix);
+            }
+        }
+        return  maxi;
+    }
+
+    // Optimal Approach using Kadanes's Alagorithm
+    static long kadanesAlgorithm(int[] arr){
+        long n = arr.length;
+        long maxi = Integer.MIN_VALUE;
+        long prefix = 0;
+
+        for(int i=0; i<n; i++){
+            prefix += arr[i];
+            maxi = Math.max(prefix, maxi);
+
+            if(prefix < 0)
+                prefix  = 0;
+
+        }
+
+        return  maxi;
+    }
+}
