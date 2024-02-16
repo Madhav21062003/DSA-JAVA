554c0b6a71f04cdde53c45630e9fa5524741b79c
Madhav Solanki <madhav09solanki@gmail.com>
2024-02-16 17:00:11 +0530

    Trapping Rainwater Problem optimised Approach

--- src/one_d_arrays/hard_problems/TrappingRainWater.java
+++ src/one_d_arrays/hard_problems/TrappingRainWater.java
@@ -6,6 +6,7 @@ public class TrappingRainWater {
         int[] height = {4,2,0,6,3,2,5};
         int totalWaterTrapped = trappedRainWater(height);
         System.out.println("Total Amount of water trapped between the buildings are = "+totalWaterTrapped);
+        System.out.println("Total Amount of water trapped between the buildings are = "+trap(height));
     }
 
     static int trappedRainWater(int[] height){
@@ -35,5 +36,36 @@ public class TrappingRainWater {
             trappedWater += waterLevel - height[i];
         }
         return trappedWater;
+    }
+
+    // Optimised approach without Using Extra space
+    public static int trap(int[] height) {
+        int n = height.length;
+        int leftMax = 0, rightMax = 0, maxHeight = height[0], index = 0;
+        int water = 0;
+
+        // Step - 1 Find the Max Height of the building
+        for(int i=1; i<n; i++){
+            if(maxHeight < height[i]){
+                maxHeight = height[i];
+                index = i;
+            }
+        }
+        // Step - 2 Solve the Left Part
+        for(int i=0; i<index; i++){
+            if(leftMax > height[i])
+                water += leftMax - height[i];
+            else
+                leftMax = height[i];
+        }
+
+        // Step - 3 solve the right part
+        for(int i=n-1; i>index; i--){
+            if(rightMax > height[i])
+                water += rightMax - height[i];
+            else
+                rightMax = height[i];
+        }
+        return water;
     }
 }
