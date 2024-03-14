4d2b54d5cb5dd11bdd9c484c5cffae8d46baa8da
Madhav Solanki <madhav09solanki@gmail.com>
2024-03-14 08:17:05 +0530

    QuickSort

---
+++ src/recursion/day_7/QuickSort.java
@@ -0,0 +1,42 @@
+package recursion.day_7;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+        int[] arr = {10,3,4,1,5,6,3,2,11,9};
+        quickSort(arr,0, arr.length-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    static void quickSort(int[] arr, int start, int end){
+
+        // Base Case
+        if (start >= end)
+            return;
+
+        // find the pivot element in the array
+        int pivot = partition(arr, start, end);
+
+        // Left Side
+        quickSort(arr,start,pivot-1);
+
+        // Right side
+        quickSort(arr,pivot+1,end);
+    }
+
+    static int partition(int[] arr, int start, int end){
+        int pos = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] <= arr[end]){
+                int temp = arr[i];
+                arr[i] = arr[pos];
+                arr[pos] = temp;
+
+                pos++;
+            }
+        }
+        return pos-1;
+    }
+}
