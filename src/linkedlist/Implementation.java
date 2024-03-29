9384c9c1ed17cb1039069e096f951920f3eb6865
Madhav Solanki <madhav09solanki@gmail.com>
2024-03-29 10:55:30 +0530

    LinkedList

--- src/linkedlist/Implementation.java
+++ src/linkedlist/Implementation.java
@@ -2,85 +2,49 @@ package linkedlist;
 
 public class Implementation {
 
-    public static class Node{
-        int data ;
-        Node next ;
-        Node(int data){
-            this.data = data ;
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
         }
     }
 
     public static void main(String[] args) {
+
         Node a = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
         Node d = new Node(4);
         Node e = new Node(5);
+        Node f = new Node(6);
 
-        // elements in the linked list are 1 2 3 4 5
-        a.next = b ;   // 1 -> 2
-        b.next = c ;   // 1 -> 2 -> 3
-        c.next = d ;   // 1 -> 2 -> 3 -> 4
-        d.next = e ;   // 1 -> 2 -> 3 -> 4 -> 5
+        a.next = b;  // 1->2
+        b.next = c;  // 1 -> 2 -> 3
+        c.next = d;  // 1 -> 2 -> 3 -> 4
+        d.next = e;  // 1 -> 2 -> 3 -> 4 -> 5
+        e.next = f;  // 1 -> 2 -> 3 -> 4 -> 5 -> 6
 
-        System.out.println(a.data);   // 1
-        System.out.println(a.next.data); // 1 -> 2
-        System.out.println(a.next.next.data);  // 1 -> 2 -> 3
-        System.out.println(a.next.next.next.data);  // 1 -> 2 -> 3 -> 4
-        System.out.println(a.next.next.next.next.data); // 1 -> 2 -> 3 -> 4 -> 5
-
-        printList(a);
+        displayList(a);
         System.out.println();
-        printListRecursively(a);
-        System.out.println();
-        reverseLL(a);
-        System.out.println();
-        int length = linkedListLength(a);
-        System.out.println(length);
+        displayListRecursively(a);
     }
-
-    static void printList(Node head){
-        Node temp = head ;
-        if (head == null)
-            return;
-
-        else {
-            while (temp != null){
-                System.out.print(temp.data+" -> ");
-                temp = temp.next ;
-                if (temp == null)
-                    System.out.print("null");
-            }
+    static void displayList(Node head){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
         }
+
     }
 
-    // Printing the linked list recursively
-    static void printListRecursively(Node head){
-        if (head == null)
-            return;
-        Node temp  = head ;
-         System.out.print(temp.data+" ");
-        printListRecursively(temp.next);
-    }
-
-    // Print the Linked List in reverse Order
-    static void reverseLL(Node head){
+    static void displayListRecursively(Node head){
         if (head == null)
             return;
 
-        reverseLL(head.next);
         System.out.print(head.data+" ");
+        displayList(head.next);
+        
     }
-
-    // Length of a Linked List
-    static int linkedListLength(Node head){
-        int count = 0 ;
-        while (head != null){
-            count ++ ;
-            head = head.next ;
-        }
-        return count;
-    }
-
-
 }
